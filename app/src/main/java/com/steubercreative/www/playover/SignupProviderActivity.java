47737e5575e441b1b1ac9cd05ad795c5999693a0
Kingdomquest <jmhudson3@crimson.ua.edu>
2017-11-16 20:00:00 -0600

    I Did Things Again.

---
+++ app/src/main/java/com/steubercreative/www/playover/SignupProviderActivity.java
@@ -0,0 +1,30 @@
+package com.steubercreative.www.playover;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignupProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_provider);
+
+        Button mSignUpProviderButton = findViewById(R.id.signup_button);
+        mSignUpProviderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView username = findViewById(R.id.user_username);
+
+                Intent intent = new Intent(SignupProviderActivity.this, LoginActivity.class);
+                intent.putExtra("Username", (String)username.getText());
+
+                startActivity(intent);
+            }
+        });
+    }
+}
