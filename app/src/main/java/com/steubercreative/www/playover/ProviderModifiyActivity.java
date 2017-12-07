dcaacaf583edfbdc344cec02940c023142125b19
DESKTOP-K5GR636\stste <ststeuber@crimson.ua.edu>
2017-12-07 13:27:44 -0600

    Caleb Did Things

--- app/src/main/java/com/steubercreative/www/playover/ProviderModifiyActivity.java
+++ app/src/main/java/com/steubercreative/www/playover/ProviderModifiyActivity.java
@@ -1,11 +1,13 @@
 package com.steubercreative.www.playover;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -13,27 +15,70 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ProviderModifiyActivity extends AppCompatActivity {
 
     private int _activityID;
     private boolean _isEdit = false;
+    final private Activity activity = new Activity();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_modify);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.custom_spinner, getResources().getStringArray(R.array.Airport_array));
-        Spinner airports = findViewById(R.id.spinner);
-        airports.setAdapter(adapter);
+
+        final AirportArray airportArray = new AirportArray();
+        final Context activityContext = this;
+
+        airportArray.onCompletion(new Procedure() {
+            @Override
+            public void run() {
+                List<String> airportStrings = new ArrayList<>();
+                for(Airport a : airportArray.getObjects())
+                    airportStrings.add(a.getCode());
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(activityContext, R.layout.custom_spinner, airportStrings);
+                Spinner airports = findViewById(R.id.spinner);
+                airports.setAdapter(adapter);
+
+            }
+        });
+        airportArray.retrieve(this);
 
         Intent parentIntent = getIntent();
         _activityID = parentIntent.getIntExtra("ActivityID", -1);
         _isEdit = _activityID != -1 ? true : false;
+        activity.setUid(_activityID);
+        if (_isEdit) {
+            activity.onCompletion(new Procedure() {
+                @Override
+                public void run() {
+                    EditText activityNameTextForm = findViewById(R.id.ActivityNameTextField);
+                    activityNameTextForm.setText(activity.getName());
 
-        if (_isEdit) PopulateFields();
+                    //must choose correct airport
+
+                    EditText locationTextForm = findViewById(R.id.LocationTextField);
+                    locationTextForm.setText(activity.getLocation());
+
+                    EditText costTextForm = findViewById(R.id.CostTextField);
+                    costTextForm.setText(String.valueOf(activity.getCost()));
+
+                    EditText durationTextForm = findViewById(R.id.DurationTextField);
+                    durationTextForm.setText(String.valueOf(activity.getDuration()));
+
+                    RadioGroup radioStatusGroup = findViewById(R.id.StatusRadioGroup);
+                    radioStatusGroup.check(R.id.InactiveRadio);
+                }
+            });
+            activity.retrieve(this);
+        }
+        else PopulateFields();
 
         RadioGroup scheduleRadioGroup = findViewById(R.id.SchedulingRadioGroup);
         scheduleRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
@@ -49,6 +94,38 @@ public class ProviderModifiyActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        Button mSignUpProviderButton = findViewById(R.id.saveButton);
+        mSignUpProviderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveActivity();
+            }
+        });
+
+    }
+    
+    private void saveActivity() {
+        activity.onCompletion(new Procedure() {
+            @Override
+            public void run() {
+                if (!activity.error()){
+                    Intent intent = new Intent(ProviderModifiyActivity.this, ManageActivitiesActivity.class);
+                    intent.putExtra("ProviderID", activity.getProviderUid());
+                    startActivity(intent);
+                }
+            }
+        });
+        if(_isEdit) {
+            updateActivity();
+            //activity.save(this);
+        }
+        else {
+            updateActivity();
+            //activity.create(this);
+        }
+
     }
 
     private void PopulateFields(){
@@ -68,5 +145,25 @@ public class ProviderModifiyActivity extends AppCompatActivity {
 
         RadioGroup radioStatusGroup = findViewById(R.id.StatusRadioGroup);
         radioStatusGroup.check(R.id.InactiveRadio);
+    }
+
+    private void updateActivity() {
+        EditText activityNameTextForm = findViewById(R.id.ActivityNameTextField);
+        activity.setName(activityNameTextForm.getText().toString());
+
+        //must choose correct airport
+
+        EditText locationTextForm = findViewById(R.id.LocationTextField);
+        activity.setLocation(locationTextForm.getText().toString());
+
+
+        EditText costTextForm = findViewById(R.id.CostTextField);
+        activity.setCost(Float.valueOf(costTextForm.getText().toString()));
+
+        EditText durationTextForm = findViewById(R.id.DurationTextField);
+        activity.setDuration(Integer.valueOf(durationTextForm.getText().toString()));
+
+        Spinner airports = findViewById(R.id.spinner);
+        activity.setAirport(airports.getSelectedItem().toString());
     }
 }
