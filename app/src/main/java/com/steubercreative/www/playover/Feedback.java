42f9dbffc048b3615f1acb4022403fa47f12e378
ProfCoffee <ststeuber@crimson.ua.edu>
2017-12-07 03:09:26 -0600

    activities

---
+++ app/src/main/java/com/steubercreative/www/playover/Feedback.java
@@ -0,0 +1,17 @@
+package com.steubercreative.www.playover;
+
+/**
+ * Created by Sean on 12/7/2017.
+ */
+
+public class Feedback extends AbstractModifiableDBObject {
+    private int uid;
+    private int author;
+    private int provider;
+    private int activity;
+    private String content;
+    private int rating;
+
+
+
+}
